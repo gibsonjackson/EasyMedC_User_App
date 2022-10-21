013b06d2e39eba2ea50a7b45e62eaaf777341743
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-10-21 09:49:31 +0530

    Thoda Thoda Start kar raha hu XD

---
+++ app/src/main/java/com/anvaishy/easymedc_user_app/HomePage/homePageView.java
@@ -0,0 +1,16 @@
+package com.anvaishy.easymedc_user_app.HomePage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anvaishy.easymedc_user_app.R;
+
+public class homePageView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_view);
+    }
+}
--- app/src/main/java/com/anvaishy/easymedc_user_app/MainActivity.java
+++ app/src/main/java/com/anvaishy/easymedc_user_app/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//This is going to be just the SplashScreen hence no ViewModel and Model for this page
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
