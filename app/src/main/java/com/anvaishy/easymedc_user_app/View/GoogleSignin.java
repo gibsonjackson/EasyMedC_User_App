6eb799f6e32a563146b1d7addf8abfa4cfae87b4
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2022-11-10 11:20:09 +0530

    Added Firebase Authentication using Google Sign In

--- app/src/main/java/com/anvaishy/easymedc_user_app/View/GoogleSignin.java
+++ app/src/main/java/com/anvaishy/easymedc_user_app/View/GoogleSignin.java
@@ -1,17 +1,103 @@
 package com.anvaishy.easymedc_user_app.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.anvaishy.easymedc_user_app.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class GoogleSignin extends AppCompatActivity {
-//This is going to be just the SplashScreen hence no ViewModel and Model for this page
+    SignInButton signInButton;
+    GoogleSignInClient googleSignInClient;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.google_signin);
 
+        signInButton = findViewById(R.id.bt_sign_in);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("2350897725-kttff7vupbvvs75m7hrif1hhnrdoc3gp.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(GoogleSignin.this, googleSignInOptions);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent, 100);
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null)
+        {
+            startActivity(new Intent(this, StudentProfile.class)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100)
+        {
+            displayToast("Google Sign In Successful!");
+            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (signInAccountTask.isSuccessful())
+            {
+                try {
+                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
+                    if (googleSignInAccount != null)
+                    {
+                        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+                        firebaseAuth.signInWithCredential(authCredential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful())
+                                        {
+                                            displayToast("Firebase Authentication Successful!");
+                                            startActivity(new Intent(GoogleSignin.this, StudentProfile.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                        }
+                                        else
+                                        {
+                                            displayToast("Authentication failed!");
+                                        }
+                                    }
+                                });
+                    }
+                }
+                catch (ApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void displayToast(String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/anvaishy/easymedc_user_app/View/StudentProfile.java
+++ app/src/main/java/com/anvaishy/easymedc_user_app/View/StudentProfile.java
@@ -1,7 +1,13 @@
 package com.anvaishy.easymedc_user_app.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+
+
 import com.anvaishy.easymedc_user_app.R;
 
 public class StudentProfile extends AppCompatActivity {
@@ -10,6 +16,9 @@ public class StudentProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
-
+        Toolbar toolbar = findViewById(R.id.simple_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Student Profile");
     }
 }
