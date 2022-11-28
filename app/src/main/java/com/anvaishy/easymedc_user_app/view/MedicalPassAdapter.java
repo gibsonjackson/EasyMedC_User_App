6952d43420c8c0519273bdcd9d48fb35d6c80943
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2022-11-28 20:41:58 +0530

    Fixed Medical Pass Requests UI

--- app/src/main/java/com/anvaishy/easymedc_user_app/view/MedicalPassAdapter.java
+++ app/src/main/java/com/anvaishy/easymedc_user_app/view/MedicalPassAdapter.java
@@ -1,5 +1,6 @@
 package com.anvaishy.easymedc_user_app.view;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.anvaishy.easymedc_user_app.R;
@@ -23,21 +25,31 @@ public class MedicalPassAdapter extends FirestoreRecyclerAdapter<MedicalPassRequ
         super(options);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onBindViewHolder(@NonNull MedicalViewHolder holder, int pos, @NonNull MedicalPassRequestUser request) {
         Log.d("ok1", request.getDescription());
         holder.description.setText(request.getDescription());
-        holder.arrival.setText(request.getArrival().toDate().toString());
-        holder.depart.setText(request.getDepart().toDate().toString());
+        StringBuilder arrivalTime = new StringBuilder(request.getArrival().toDate().toString());
+        arrivalTime.delete(20, 30);
+        StringBuilder departTime = new StringBuilder(request.getDepart().toDate().toString());
+        departTime.delete(20, 30);
+        holder.arrival.setText(arrivalTime.toString());
+        holder.depart.setText(departTime.toString());
         holder.arrival.setTextColor(Color.BLACK);
         holder.depart.setTextColor(Color.BLACK);
         if(request.getStatus()==2){
-            holder.status.setCardBackgroundColor(Color.RED);
+            holder.status.setCardBackgroundColor(Color.rgb(228, 59, 59));
+            holder.description.setTextColor(Color.rgb(224, 227, 231));
+            holder.arrival.setTextColor(Color.rgb(224, 227, 231));
+            holder.depart.setTextColor(Color.rgb(224, 227, 231));
         }
         else if(request.getStatus()==1){
-            holder.status.setCardBackgroundColor(Color.GREEN);
+            holder.status.setCardBackgroundColor(Color.rgb(72, 198, 57));
         }
-
+        else if (request.getStatus() == 0) {
+            holder.status.setCardBackgroundColor(Color.rgb(224, 227, 231));
+        }
     }
 
     @NonNull
